6d3170cfe5efca80d51f95b0bd4db34bb1daad33
Mike Adams Mwambia <mikemwambia@gmail.com>
2018-12-04 18:11:31 +0800

    Initial commit

--- truffle.js
+++ truffle.js
@@ -1,9 +1,14 @@
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
+  networks : {
+	  development : {
+		  host:"127.0.0.1",
+		  port:8545,
+		  network_id : "*"
+  },
+    ganache: {
+      host: "127.0.0.1",
       port: 8545,
-      network_id: "*" // Match any network id
-    }
+      network_id: "*"
   }
+}
 };
